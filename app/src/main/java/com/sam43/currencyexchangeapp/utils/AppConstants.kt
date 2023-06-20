ffbb1f547fa0565d44837733a83344a9c5d0f31c
A S M Sayem <scode43@gmail.com>
2023-06-20 21:02:30 +0900

    v1.0.1_Unit_Testing - Refactored and cleaned up - Fixed bug for empty or null values

--- app/src/main/java/com/sam43/currencyexchangeapp/utils/AppConstants.kt
+++ app/src/main/java/com/sam43/currencyexchangeapp/utils/AppConstants.kt
@@ -1,10 +1,12 @@
 package com.sam43.currencyexchangeapp.utils
 
 object AppConstants {
+    const val HTTP_EXCEPTION_ERROR: String = "Oops, Some error occurred while parsing the response!"
     const val INVALID_AMOUNT: String = "Not a valid amount"
     const val LOADING = "Loading..."
     const val DEFAULT_CURRENCY = "USD"
     const val DEFAULT_VALUE = "1.0"
+    const val NO_VALUE = "0.0"
     const val WATCHER_DELAY = 300L
     const val INTERNET_CONNECTION_ERROR = "Couldn't reach server, check your internet connection."
 
